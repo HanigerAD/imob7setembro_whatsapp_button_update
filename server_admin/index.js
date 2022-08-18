ed02fcdf6bf55a94b653e1e6eba7bd07fb33fdea
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2022-08-18 16:23:20 -0300

    servidor de admin

---
+++ server_admin/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("CDN conectado na porta:", PORT);
+});
