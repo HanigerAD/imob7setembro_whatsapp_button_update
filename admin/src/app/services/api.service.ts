e2500e3cbc0f5f31d94dc6076416ca4cc06df2ed
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2022-08-26 08:53:48 -0300

    documentos do imovel - frontend

--- admin/src/app/services/api.service.ts
+++ admin/src/app/services/api.service.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth.service";
 
 const apiService = axios.create({
-  // baseURL: "http://xbcopu.hospedagemelastica.com.br",
-  baseURL: "http://localhost:3000",
+  baseURL: "http://xbcopu.hospedagemelastica.com.br",
+  // baseURL: "http://localhost:3000",
   headers: {
     "Content-type": "application/json",
   },
