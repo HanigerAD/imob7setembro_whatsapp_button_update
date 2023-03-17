ff69c7093a51575c4fc2db25d89d6ceeccb7da2d
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2023-03-17 11:01:36 -0300

    redirecionamento de login

--- api/src/authentication/controller/authentication.controller.ts
+++ api/src/authentication/controller/authentication.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { LoginRequest } from '../integration/request/login.request';
 import { LoginResponse } from '../integration/response/login.response';
 import { AuthenticationService } from '../service/authentication.service';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../config/jwt-auth.guard';
 
 @ApiTags('Autenticação')
 @Controller('auth')
@@ -15,4 +16,8 @@ export class AuthenticationController {
   public async login(@Body() request: LoginRequest): Promise<LoginResponse> {
     return this.service.login(request);
   }
+
+  @Get('/token-verify')
+  @UseGuards(JwtAuthGuard)
+  public async tokenVerify(): Promise<void> {}
 }
