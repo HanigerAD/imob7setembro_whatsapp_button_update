c7ce9ad33bb69576d5427cf89cd4ad547e7b80f4
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2023-03-17 15:27:20 -0300

    melhoria em filtros de imoveis

--- api/src/property/property/mapper/property-filter.mapper.ts
+++ api/src/property/property/mapper/property-filter.mapper.ts
@@ -6,47 +6,47 @@ import { PropertyFilterRequest } from '../integration/request/property-filter.re
 import { Builder } from 'builder-pattern';
 
 export class PropertyFilterMapper {
-    public static requestToEntity(request: PropertyFilterRequest): PropertyFilterEntity {
-        return Builder<PropertyFilterEntity>()
-            .codigo_interno(request?.internalCode)
-            .transacao(request?.finality ? Number(request?.finality) : null)
-            .bairro(this.validateNeighborhoods(request.neighborhood))
-            .hectare(request?.hectare)
-            .zona(request?.zone)
-            .codigo(request?.code)
-            .municipio(request?.city)
-            .tipo(request?.type)
-            .paginacao(this.generatePaginationFilter(request))
-            .preco(this.generatePriceFilter(request))
-            .financiavel(request?.financeable)
-            .exibir(request?.showSite)
-            .categoria(request.category)
-            .zona(request.zone)
-            .dormitorio(request?.bedroom)
-            .vaga(request?.parkingVacancy)
-            .banheiro(request?.bathroom)
-            .build();
-    }
+  public static requestToEntity(request: PropertyFilterRequest): PropertyFilterEntity {
+    return Builder<PropertyFilterEntity>()
+      .codigo(request?.code)
+      .codigo_interno(request?.internalCode)
+      .transacao(request?.finality ? Number(request?.finality) : null)
+      .bairro(this.validateNeighborhoods(request.neighborhood))
+      .hectare(request?.hectare)
+      .zona(request?.zone)
+      .municipio(request?.city)
+      .tipo(request?.type)
+      .paginacao(this.generatePaginationFilter(request))
+      .preco(this.generatePriceFilter(request))
+      .financiavel(request?.financeable)
+      .exibir(request?.showSite)
+      .categoria(request.category)
+      .zona(request.zone)
+      .dormitorio(request?.bedroom)
+      .vaga(request?.parkingVacancy)
+      .banheiro(request?.bathroom)
+      .build();
+  }
 
-    public static validateNeighborhoods(neighborhoods: any): any {
-        if (neighborhoods?.length == 1) {
-            return Array.of(neighborhoods);
-        } else if (neighborhoods?.length > 1) {
-            return neighborhoods;
-        }
+  public static validateNeighborhoods(neighborhoods: any): any {
+    if (neighborhoods?.length == 1) {
+      return Array.of(neighborhoods);
+    } else if (neighborhoods?.length > 1) {
+      return neighborhoods;
     }
+  }
 
-    public static generatePriceFilter(filter: PropertyFilterRequest): PriceFilterEntity {
-        return Builder<PriceFilterEntity>()
-            .minPrice(filter.minPrice ? Number(filter.minPrice) : 0)
-            .maxPrice(filter.maxPrice ? Number(filter.maxPrice) : 10000000)
-            .build();
-    }
+  public static generatePriceFilter(filter: PropertyFilterRequest): PriceFilterEntity {
+    return Builder<PriceFilterEntity>()
+      .minPrice(filter.minPrice ? Number(filter.minPrice) : 0)
+      .maxPrice(filter.maxPrice ? Number(filter.maxPrice) : 100000000000)
+      .build();
+  }
 
-    public static generatePaginationFilter(filter: PropertyFilterRequest): PaginationEntity {
-        return Builder<PaginationEntity>()
-            .pagina(filter.page ?? 1)
-            .porPagina(filter.perPage ?? 1000000)
-            .build();
-    }
+  public static generatePaginationFilter(filter: PropertyFilterRequest): PaginationEntity {
+    return Builder<PaginationEntity>()
+      .pagina(filter.page ?? 1)
+      .porPagina(filter.perPage ?? 100000000000)
+      .build();
+  }
 }
